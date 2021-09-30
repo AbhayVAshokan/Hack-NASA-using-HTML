2672d1306da8bef61dbd2f56b51a477a74dba700
AbhayVAshokan <abhayvashokan@gmail.com>
2021-09-30 18:12:57 +0530

    Added css minifier plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,39 @@
-const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const mode = process.env.NODE_ENV || "development";
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({ template: "./src/index.html" }),
+];
+
+if (mode === "production") {
+  plugins.push(new CssMinimizerPlugin());
+}
 
 module.exports = {
   mode: mode,
   entry: "./src/index.js",
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[hash][ext][query]",
-  },
-
   module: {
     rules: [
       {
+        test: /\.css/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: { publicPath: "" },
-          },
-          "css-loader",
-          "postcss-loader",
-        ],
-      },
-      {
-        test: /.(png|jpe?g|gif|svg)$/i,
-        exclude: /node_modules/,
-        type: "asset/resource",
+        use: { loader: "babel-loader" },
       },
     ],
   },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-  ],
+  plugins: plugins,
 
   devtool: "source-map",
   devServer: {
